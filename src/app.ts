20f0592c50d5e2a64fb2a085a828284bacc4b805
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-01-17 10:11:06 -0600

    Start to express app.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import express, {Application} from "express";
+import logger from 'morgan';
+import path from 'path';
 
 export const app: Application = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res)=>{
     res.send("Hello World!");
