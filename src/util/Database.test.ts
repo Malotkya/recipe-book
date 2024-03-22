347b3db6fe2bd186158ffe263bb49c2a0af0be9f
Malotkya <47186158+Malotkya@users.noreply.github.com>
2024-03-22 09:53:00 -0500

    Wrote a couple of more tests for Database.

--- src/util/Database.test.ts
+++ src/util/Database.test.ts
@@ -1,4 +1,5 @@
 import Database from "./Database";
+import { DatabaseDocument } from "./Database/Types";
 
 test("Database Init Test", ()=>{
     new Database("test");
@@ -19,7 +20,7 @@ test("Collection Init Test", async()=>{
     const database = new Database("test");
 
     const collection = database.collection("newCollection");
-    expect(Number(await collection.getNewRef())).toBeCloseTo(Date.now(), 100);
+    expect(Number(await collection.getNewRef())).toBeCloseTo(Date.now());
 });
 
 test("Collection Insert Test", async()=>{
@@ -43,4 +44,42 @@ test("Collection Update Test", async()=>{
 
     await collection.updateDocument(document);
     expect(await collection.count()).toBe(1);
+});
+
+test("Collection Reference Test", async()=>{
+    const database = new Database("test");
+    const collection = database.collection("referenceTest");
+    collection.clear();
+
+    let document:DatabaseDocument|null = await collection.insertDocument({Hello: "World"});
+    document.data.Hello = "Dont See this Update";
+
+    document = await collection.getDocument(document.ref);
+    if(document)
+        expect(document.data.Hello).toBe("World");
+    else
+        throw new Error("Document was not Found!");
+});
+
+test("Delete Test", async()=>{
+    const database = new Database("test");
+    const collection = database.collection("deleteTest");
+    const data = {name: "Ben Wyatt", location: "Pawni, IN"};
+
+    const document = await collection.insertDocument(data);
+    await collection.deleteDocument(document);
+    expect(await collection.count()).toBe(0);
+
+    const {ref} = await collection.insertDocument(data);
+    await collection.deleteDocument(ref);
+    expect(await collection.count()).toBe(0);
+})
+
+test("Collection First Query Test", async()=>{
+    const database = new Database("test");
+    const collection = database.collection("updateTest");
+    const documents = await collection.query();
+
+    expect(documents.length).toBe(1);
+    expect(documents[0].data.Hello).toBe('I have been updated');
 });
